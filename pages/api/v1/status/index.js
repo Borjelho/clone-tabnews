c284abaedf2024205df78e43b5a67bfa7db23dd5
Borjelho <lucasbrjhl@gmail.com>
2026-01-06 15:30:11 -0300

    add `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
-  response.status(200).json({ resposta: "ta na mao meu chapo" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ resposta: "ta na mao meu schapoo" });
 }
 export default status;
