0396147a46cf1b9b7cde1912f7a601ae849c2528
Borjelho <lucasbrjhl@gmail.com>
2026-01-17 11:01:45 -0300

    fix endpoint `/api/v1/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,20 +3,28 @@ import { join } from "node:path";
 import database from "infra/database.js";
 import { error } from "node:console";
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method ${request.method} not allowed`,
+    });
+  }
+
+  let dbClient;
 
   try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
     if (request.method === "GET") {
       const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-      await dbClient.end();
       return response.status(200).json(pendingMigrations);
     }
     if (request.method === "POST") {
@@ -24,7 +32,6 @@ export default async function migrations(request, response) {
         ...defaultMigrationOptions,
         dryRun: false,
       });
-      await dbClient.end();
       if (migratedMigrations.length > 0) {
         return response.status(201).json(migratedMigrations);
       }
@@ -32,8 +39,8 @@ export default async function migrations(request, response) {
     }
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await dbClient.end();
-    return response.status(405).end();
   }
 }
