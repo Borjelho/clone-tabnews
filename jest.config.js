24afd93cf00a95e85fa885ce833f23382fca51c6
Borjelho <lucasbrjhl@gmail.com>
2026-01-22 09:36:48 -0300

    faz a bateria de testes ser mais robusta com `async-retry` e `orchestrator.js`

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,6 @@ dotenv.config({
 const createJestConfig = nextJest({ dir: "." });
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 60000,
 });
 module.exports = jestConfig;
